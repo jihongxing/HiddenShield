60c48cc0485e159fce792f93e074059bef3269fc
JHX800 <jhongxing705@gmail.com>
2026-06-16 17:54:07 +0800

    Fix mobile web preview storage

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import 'app/app.dart';
 import 'app/bootstrap.dart';
@@ -8,7 +9,10 @@ import 'storage/vault_store.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final bridge = await createDefaultWatermarkBridge();
-  final appState = MobileAppState(vaultStore: await SQLiteVaultStore.open());
+  final vaultStore = kIsWeb
+      ? MemoryVaultStore()
+      : await SQLiteVaultStore.open();
+  final appState = MobileAppState(vaultStore: vaultStore);
   await appState.load();
   runApp(HiddenShieldApp(bridge: bridge, appState: appState));
 }
